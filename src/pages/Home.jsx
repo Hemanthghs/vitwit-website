70d0170be19932cc4d0ca5228deb8c8ac6c358c8
hemanthghs <ghshemanth@gmail.com>
2023-09-23 18:28:54 +0530

    wip

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,22 +15,22 @@ const Home = () => {
             </h1>
             <div className="home__first__container__1__text">
               <p>
-                <div className="mb-[22px]">
+                <div className="mb-3 lg:mb-[22px]">
                   <span>We&nbsp;are&nbsp;offering</span>
-                  <span className="bg-[#F8CFA566] ml-4 py-[15px] rounded-2xl px-7">Staking&nbsp;Infrastructure&nbsp;Service</span>
+                  <span className="bg-[#F8CFA566] ml-1 lg:ml-4 topic">Staking&nbsp;Infrastructure&nbsp;Service</span>
                 </div>
-                <div className="mb-[22px]">
-                  <span className="bg-[#DCB8F566] mr-4 py-[15px] rounded-2xl px-7">Development&nbsp;Service</span>
+                <div className="mb-3 lg:mb-[22px]">
+                  <span className="bg-[#DCB8F566] mr-1 lg:mr-4 topic">Development&nbsp;Service</span>
                   <span>Blockchain&nbsp;Research</span>
                 </div>
-                <div className="mb-[22px]">
+                <div className="mb-3 lg:mb-[22px]">
                   <span>and</span>
-                  <span className="bg-[#BCDCF766] mx-4 py-[15px] rounded-2xl px-7">Service&nbsp;Strategy&nbsp;&&nbsp;execution</span>
+                  <span className="bg-[#BCDCF766] mx-1 lg:mx-4 topic">Service&nbsp;Strategy&nbsp;&&nbsp;execution</span>
                   <span>Along&nbsp;with</span>
                 </div>
                 <div>
-                  <span>our&nbsp;</span>
-                  <span className="bg-[#ECF8A566] ml-4 py-[15px] rounded-2xl px-7">Block&nbsp;Explorer</span>
+                  <span>our</span>
+                  <span className="bg-[#ECF8A566] ml-1 lg:ml-4 topic">Block&nbsp;Explorer</span>
                 </div>
               </p>
             </div>
