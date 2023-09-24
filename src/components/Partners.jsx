6d2ab287617f3108d1610a587b886a582f616f3e
hemanthghs <ghshemanth@gmail.com>
2023-09-24 11:49:12 +0530

    Add partners

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -5,7 +5,12 @@ const Partners = () => {
   return (
     <div className='home__partners'>
         <h1>Partners</h1>
-        <div className="home__partners__list">
+        <div className="home__partners__list w-[80%] mobile:w-fit">
+            <PartnerCard />
+            <PartnerCard />
+            <PartnerCard />
+            <PartnerCard />
+            <PartnerCard />
             <PartnerCard />
         </div>
     </div>
